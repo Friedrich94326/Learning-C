dc81f9974f2faa4658fef9c7c84987960189cb32
Friedrich_Cheng94 <48461667+Friedrich94326@users.noreply.github.com>
2020-05-10 21:04:39 +0800

    Add files via upload

--- Main_Function.c
+++ Main_Function.c
@@ -1,188 +1,278 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
+void ReverseArray(int* pArray, int LengthA)
+{
+	if (NULL == pArray)
+		return;
 
-void BubbleSortAscend(int* pArray, int ASize)
-{
-	int i, j, temp;
-	for (i = ASize - 2; i >= 0; --i)
-	{
-		for (j = 0; j <= i; ++j) // j runs over 0 up to i from the left
-		{
-			if (*(pArray + j) > * (pArray + j + 1))
-			{
-				// swap the values of num[j] & num[j+1]
-				temp = *(pArray + j);
-				*( pArray + j ) = *(pArray + j +1 );
-				*(pArray + j + 1 ) = temp;
-			}
-		}
-	}
-}
-void BubbleSortDescend(int* pArray, int ASize)
-{
-	int i, j, temp;
-	for (i = 0; i < ASize - 1; ++i)
-	{
-		for (j = i; j >= 0; --j) // j runs over 0 to i from the right
-		{
-			if (*(pArray + j) < * (pArray + j + 1))
-			{
-				// swap the values of num[j] & num[j+1]
-				temp = *(pArray + j);
-				*(pArray + j) = *(pArray + j + 1);
-				*(pArray + j + 1) = temp;
-			}
-		}
-	}
-}
+	int* pStart = pArray; // pStart points to the address of the first integer in pArray
+	int* pEnd = pStart + LengthA - 1; // pEnd points to the address of the last entry in the array
 
-void ReverseNumbers(int* pArray, int ASize)
-{
-	int* pStart = pArray;
-	int* pEnd = pStart + ASize - 1;
-	int temp;
-	
-	while (pStart <= pEnd)
+	while (pEnd > pStart)
 	{
-		temp = *pStart;
+		// swap the addresses by the 2 pointers
+		int temp = *pStart;
 		*pStart = *pEnd;
 		*pEnd = temp;
+
 		++pStart;
 		--pEnd;
 	}
+	return;
 }
+
 
 int main(void)
 {
-	/* bubble-sorting */
-
-	int N;
+	/* Ex 5.1: compute n^i */
+	int n;
 	int i;
-	int num[100];
+	int x = 1;
 
-	printf("How many integers will be sorted? (at max 100)");
-	scanf_s("%d", &N);
+	printf("Key in the value of base n: ");
+	scanf_s("%d", &n);
+	printf("Key in the value of power i ");
+	scanf_s("%d", &i);
 
-	for (i = 0; i < N; ++i)
+	if ((i == 0) && (n == 0))
 	{
-		printf("Thie %d-th intger: ", i + 1);
-		scanf_s("%d", &num[i]);
+		printf("This ain't work with the undefined expression 0^0!\n");
+		return 1;
 	}
-
-	printf("Do you prefer to the ascending order or the descending order? (Enter 'a' or 'd') ");
-	char order;
-	scanf_s(" %c", &order);  // char input shall be replaced with string input later!
-
-	if (order == 'a')
+	else if ((i == 0) && (n != 0))
 	{
-		BubbleSortAscend(num, N);
-	}
-	else if (order == 'd')
-	{
-		BubbleSortDescend(num, N);
+		x = 1;
 	}
 	else
 	{
-		printf("Invalid command detected!");
+		int count;
+		for (count = 0; count < i; ++count)
+		{
+			x *= n;
+		}
 	}
+	printf("x = %d \n", x);
+
+
+	/* Ex 5.5: Give serial commands to a moving vehicle.  And print out the final position of that car. */
+	int N;
+	int posX;
+	int posY;
+	int cmd[200]; // array of commands
+	char dir = 'n'; // direction indicator
+	int v; // constant speed
+	int dt;
+
+	printf("Give the original position of the vehicle. \n posX0 = ");
+	//printf("%d", posX);
+	scanf_s("%d", &posX);
+	printf(" posY0 = ");
+	//printf("%d\n", posY);
+	scanf_s("%d", &posY);
+
+	printf("In which direction is the car currently heading? (n/e/s/w) ");
+	scanf_s(" %c", &dir);
+	printf("Also, the car shall move at a constant speed.  How fast do you wish it will move? ");
+	scanf_s("%d", &v);
+
+	printf("Give the number of commands N (N <= 100): ");
+	scanf_s("%d", &N);
+	//printf("%d \n", N);
+
 
 	for (i = 0; i < N; ++i)
 	{
-		printf("%d\n", num[i]);
+		switch ((i % 2) == 0)
+		{
+		case 1: // even i
+			printf("The %d-th command to indicate time: ", i);
+			scanf_s("%d", &cmd[i]);
+			break;
+		default: // odd i
+			printf("The %d-th command to change direction: ", i);
+			scanf_s("%d", &cmd[i]);
+		}
+		printf("cmd[%d] = %d \n", i, cmd[i]);
+
 	}
 
+	/*
+	cmd[0] = 5;
+	cmd[1] = 1;
+	cmd[2] = 10;
+	cmd[3] = 2;
+	cmd[4] = 20;
+	cmd[5] = 3;  // for debugging
+	*/
 
-	/* Ex 6.1: Reverse numbers */
-	printf("Enter the length of the array of integers. (do not exceed 100)");
+	for (i = 0; i < N; ++i)
+	{
+		if ((i % 2 != 0) && (cmd[i] != 3)) // position changes
+		{
+			if (i < 2)
+			{
+				dt = cmd[0];
+			}
+			else
+			{
+				dt = cmd[i - 1] - cmd[i - 3];
+			}
+			switch (dir)
+			{
+			case 'n':
+				posX = posX;
+				posY += dt * v;
+				if (cmd[i] == 1)
+				{
+					dir = 'w'; // turn left to head the west 
+				}
+				else
+				{
+					dir = 'e'; // turn right to head the east
+				}
+				break;
+			case 'e':
+				posX += dt * v;
+				posY = posY;
+				if (cmd[i] == 1)
+				{
+					dir = 'n'; // turn left to head the north 
+				}
+				else
+				{
+					dir = 's'; // turn right to head the south
+				}
+				break;
+			case 's':
+				posX = posX;
+				posY -= dt * v;
+				if (cmd[i] == 1)
+				{
+					dir = 'w'; // turn left to head the east 
+				}
+				else
+				{
+					dir = 'e'; // turn right to head the west
+				}
+				break;
+			case 'w':
+				posX -= dt * v;
+				posY = posY;
+				if (cmd[i] == 1)
+				{
+					dir = 's'; // turn left to head the north 
+				}
+				else
+				{
+					dir = 'n'; // turn right to head the south
+				}
+				break;
+			default:
+				printf("Invalid direction!\n");
+			}
+			printf("i = %d: cmd[%d] = %d \n", i, i, cmd[i]);
+			printf("And the car moves to (%d, %d) and head to %c. \n", posX, posY, dir);
+		}
+		else if ((i % 2 != 0) && (cmd[i] == 3))
+		{
+			switch (dir)
+			{
+			case 'n':
+				posX = posX;
+				posY += dt * v;
+				break;
+			case 'e':
+				posX += dt * v;
+				posY = posY;
+				break;
+			case 's':
+				posX = posX;
+				posY -= dt * v;
+				break;
+			case 'w':
+				posX -= dt * v;
+				posY = posY;
+				break;
+			default:
+				printf("Invalid direction!\n");
+			}
+			printf("i = %d: cmd[%d] = %d \n", i, i, cmd[i]);
+			printf("And the car moves to (%d, %d) and head to %c. \n", posX, posY, dir);
+			break;
+		}
+		else
+		{
+			printf("i = %d: cmd[%d] = %d, dir = %c \n", i, i, cmd[i], dir);
+			continue;
+		}
+	}
+	printf("The stupid vehicle was pulled over at the location of (%d, %d).\n", posX, posY);
+
+
+	/* Ex 5.10 */
+	// ¥Î¦P¾lªº·§©À!!
+	printf("Enter a natural number N: ");
 	scanf_s("%d", &N);
 
-	int arr[100];
-	
-	printf("Before reversion:\n");
-	for (i = 0; i < N; ++i)
+	int dig[100]; // digits of N
+	int count = 0;
+	do
 	{
-		printf("arr[%d] = ", i);
-		scanf_s("%d", &arr[i]);
+		dig[count] = (N % 10);
+		N /= 10;
+		++count;
+	} while (N > 0);   // extraction of all the digits 
+	printf("count = %d \n", count);
+
+	/*
+	for (i = 0; i < count; ++i)
+	{
+		printf("dig[%d] = %d \n", i, dig[i]);
 	}
-	ReverseNumbers(arr, N);
+	*/
 
-	printf("After reversion:\n");
-	for (i = 0; i < N; ++i)
+	ReverseArray(&dig[0], count);
+
+	for (i = 0; i < count; ++i)
 	{
-		printf("arr[%d] = %d \n", i, arr[i]);
-	}
-
-	/* Ex 6.3: Numbers divided into 2 groups in terms of odd and even numbers */
-
-	printf("How many integers are you gonna enter? ");
-	scanf_s("%d", &N);
-
-	for (i = 0; i < N; ++i)
-	{
-		printf("The %d-th number is set to: ", i + 1);
-		scanf_s("%d", &arr[i]);
+		printf("dig[%d] = %d \n", i, dig[i]);
 	}
 
-	for (i = 0; i < N; ++i)
+
+	printf("How many times do you implement this reversion of digits? ");
+	scanf_s("%d", &n);
+
+	int k = 0;
+
+	do
 	{
-		if (arr[i] % 2 != 0)
+		printf("The %d-th reversion:  ", k);
+		for (i = 0; i < count; ++i)
 		{
-			printf("%d ", arr[i]);
+			printf("%d", dig[i]);
 		}
-	}
-	printf("\n");
+		printf("\n");
 
-	for (i = 0; i < N; ++i)
-	{
-		if (arr[i] % 2 == 0)
+		if (dig[count - 1] == 0)
 		{
-			printf("%d ", arr[i]);
+			--count;
+			continue;
 		}
-	}
-	printf("\n");
-
-	/* Ex 6.4: averaged credits over students and courses */
-	int s;
-	int c;
-	printf("How many attendants? And how many courses do their take? (both less or equal to 100)");
-	scanf_s("%d %d", &s, &c);
-	
-	int credits[100][100];
-	int j;
-
-	for (i = 0; i < s; ++i)
-	{
-		printf("The %d-th student got his credits as follows: \n", i + 1);
-		for (j = 0; j < c; ++j)
+		int temp = dig[count - 1];
+		for (i = count - 1; i > 0; --i)
 		{
-			scanf_s("%d", &credits[i][j]);
+			dig[i] = dig[i - 1];
 		}
-	}
 
-	float avg; 
+		dig[i] = temp;
 
-	// average over courses
-	for (i = 0; i < s; ++i)
-	{
-		for (j = 0, avg = 0; j < c; ++j)
-		{
-			avg += credits[i][j];
-		}
-		avg /= c; 
-		printf("The %d-th student got his averaged score: %2f\n", i + 1, avg);
-	}
+		++k;
+	} while (k <= n);
 
-	// average over students
-	for (j = 0; j < c; ++j)
-	{
-		for (i = 0, avg = 0; i < s; ++i)
-		{
-			avg += credits[i][j];
-		}
-		avg /= s;
-		printf("The average score in the %d-th course: %2f\n", j + 1, avg);
-	}
+
 
 	return 0;
 }
+
